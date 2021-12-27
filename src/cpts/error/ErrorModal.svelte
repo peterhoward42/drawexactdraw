cd19538eadc3e30359014471569437d3b911e077
peterhoward42 <peterhoward42@gmail.com>
2021-12-27 20:43:50 +0000

    Dismiss error modal on click outside

--- src/cpts/error/ErrorModal.svelte
+++ src/cpts/error/ErrorModal.svelte
@@ -1,20 +1,32 @@
-<script>   
-    import { errorMessage } from "./errorstore.js"
+<script>
+    import { errorMessage } from "./errorstore.js";
+
+    // We bind to this variable in the html below to provide access to 
+    // this top level component.
+    let thisErrorModalCpt
+
+    // If the modal has siezed focus, then we interpret a click anwhere outside
+    // of it, as a signal to dismiss it.
+    window.onclick = function (event) {
+        if (event.target == thisErrorModalCpt) {
+            thisErrorModalCpt.style.display = "none";
+        }
+    };
 </script>
 
 <style>
 </style>
 
-<div id="errormodalcpt" class="w3-modal">
+<div id="errormodalcpt" class="w3-modal" bind:this={thisErrorModalCpt}>
     <div class="w3-modal-content">
         <header class="w3-container w3-teal">
             <span
                 onclick="document.getElementById('errormodalcpt').style.display='none'"
                 class="w3-button w3-display-topright">&times;</span>
-            <h3>Error</h3>
+            <h4>Error</h4>
         </header>
-        <div class="w3-container">
-            <p> { $errorMessage } </p>
+        <div class="w3-container w3-yellow">
+            <p>{$errorMessage}</p>
         </div>
     </div>
 </div>
