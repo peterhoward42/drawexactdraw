2c138af644f5294800cb9d0931deed08a107986f
peterhoward42 <peterhoward42@gmail.com>
2022-01-14 08:45:25 +0000

    Put in line style menu

--- src/cpts/properties/LineStyleMenu.svelte
+++ src/cpts/properties/LineStyleMenu.svelte
@@ -1,7 +1,96 @@
 <script>
     import { currentLineStyle } from "./propertiesstore";
+
+    $: localLineStyle = $currentLineStyle
+
+    function handleWeight() {
+        // Calculate the next (nudged) line weight and update the style in the
+        // store accordingly.
+        let curWeight = localLineStyle.Style.Weight
+        let newWeight = (curWeight + 1) % 3
+        localLineStyle.Style.Weight = newWeight
+        $currentLineStyle = localLineStyle
+
+        // Message WASM to tell it that the user changed the line style.
+        // Nb. The payload includes the target element to edit in-situ.
+        msgBusPubString("ui:changedlinestyle", JSON.stringify(localLineStyle))
+    }
+
+    function handleStartArrow() {
+        // Calculate the next (nudged) end terminator style and update the style in the
+        // store accordingly.
+        let curTerm = localLineStyle.Style.StartTerm
+        let newTerm = (curTerm + 1) % 3
+        localLineStyle.Style.StartTerm = newTerm
+        $currentLineStyle = localLineStyle
+
+        // Message WASM to tell it that the user changed the line style.
+        // Nb. The payload includes the target element to edit in-situ.
+        msgBusPubString("ui:changedlinestyle", JSON.stringify(localLineStyle))
+    }
+
+    function handleEndArrow() {
+        // Calculate the next (nudged) end terminator style and update the style in the
+        // store accordingly.
+        let curTerm = localLineStyle.Style.EndTerm
+        let newTerm = (curTerm + 1) % 3
+        localLineStyle.Style.EndTerm = newTerm
+        $currentLineStyle = localLineStyle
+
+        // Message WASM to tell it that the user changed the line style.
+        // Nb. The payload includes the target element to edit in-situ.
+        msgBusPubString("ui:changedlinestyle", JSON.stringify(localLineStyle))
+    }
+
+    function handleDashes() {
+        // Calculate the next (nudged) dash style and update the style in the
+        // store accordingly.
+        let curDashes = localLineStyle.Style.FullOrDashed
+        let newDashes = (curDashes + 1) % 3
+        localLineStyle.Style.FullOrDashed = newDashes
+        $currentLineStyle = localLineStyle
+
+        // Message WASM to tell it that the user changed the line style.
+        // Nb. The payload includes the target element to edit in-situ.
+        msgBusPubString("ui:changedlinestyle", JSON.stringify(localLineStyle))
+    }
 </script>
 
-<div>
-    hello {JSON.stringify($currentLineStyle)}
+<div class="menu">
+    <div class="label">Thickness</div>
+    <div class="btn" on:click={handleWeight}>&#x00BB;</div>
+
+    <div class="label">Dash Pattern</div>
+    <div class="btn" on:click={handleDashes}>&#x00BB;</div>
+
+    <div class="label">Start Arrow</div>
+    <div class="btn" on:click={handleStartArrow}>&#x00BB;</div>
+
+    <div class="label">End Arrow</div>
+    <div class="btn" on:click={handleEndArrow}>&#x00BB;</div>
 </div>
+
+<style>
+    .menu {
+        display: grid;
+        gap: 0px 5px;
+        grid-template-columns: auto auto ; 
+        align-items: center;
+        background-color: #333;
+        padding: 5px;
+    }
+    .menu .btn {
+        font-size: 16px;
+        color: white;
+        background-color: #666;
+        margin: 3px;
+        padding-left: 7px;
+        padding-right: 7px;
+        padding-top: 0px;
+        padding-bottom: 0px;
+    }
+    .menu .btn:hover {
+        background-color: #444;
+    }
+
+</style>
--- src/cpts/properties/menulaunch.js
+++ src/cpts/properties/menulaunch.js
@@ -4,7 +4,11 @@ import { sideBarComponent } from "../sidebar/sidebarstore.js"
 import LineStyleMenu from "./LineStyleMenu.svelte"
 
 function bringUpLineStyleMenu(payloadJSON) {
+    // Update the stored current line style accordingly.
     let payloadObj = JSON.parse(payloadJSON);
     currentLineStyle.set(payloadObj);
+
+    // And update the store's sideBarComponent to be the linestyle menu.
+    // That will cause the sideBarComponent to appear, and render the menu.
     sideBarComponent.set(LineStyleMenu);
 }
