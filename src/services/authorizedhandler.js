cee55def888ab2074e28b5ee795e90f4619ef33a
peterhoward42 <peterhoward42@gmail.com>
2022-03-25 21:58:38 +0000

    Route to /authorized on sign in success

--- src/services/authorizedhandler.js
+++ src/services/authorizedhandler.js
@@ -1,3 +1,4 @@
+import page  from "page";
 
 // This function is provided as a callback to the firebaseui. It gets
 // called IFF the user successfully signs in.
@@ -5,5 +6,7 @@ export function handleSignInSuccess(authResult) {
     console.log("XXXX sign in callback fired with authResult: ", authResult)
     // Returning false tells the firebaseui UI that we DO NOT want *it* to mandate
     // the browser to redirect to a different page. (We'll handle that ourselves).
+
+    page("/authorized")
     return false;
 }
