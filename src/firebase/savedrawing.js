e4a7a58b6babc6123e29b13ccb30a6cf826f99fe
peterhoward42 <peterhoward42@gmail.com>
2022-04-14 09:59:39 +0100

    Correct previous commit

--- src/firebase/savedrawing.js
+++ src/firebase/savedrawing.js
@@ -1,15 +1,12 @@
 import { uploadBlob } from "./uploadblob.js";
-import { setCurrentDrawing } from "./setcurrentdwg";
 import { firebaseAuth } from "./store.js"
 import { get } from 'svelte/store';
 
 // saveDrawing (asyncrounously) uploads the given drawing
-// to Firebase storage for the signed in user, *AND* marks it as the
-// user's current drawing.
+// to Firebase storage for the signed in user,
 export function saveDrawing(drawingName, serializedDrawingAsBlob) {
     const auth = get(firebaseAuth)
     const uid = auth.currentUser.uid;
     const path = 'user/' + uid  + '/drawings/' + drawingName
     uploadBlob(path, serializedDrawingAsBlob)
-    setCurrentDrawing(drawingName)
 }
--- src/services/save.js
+++ src/services/save.js
@@ -1,11 +1,13 @@
 import { saveDrawing } from "../firebase/savedrawing";
+import { setCurrentDrawing } from "../firebase/setcurrentdwg";
 
 // saveSerializedDrawing extracts the drawing name, and the serialized
 // drawing bytes from the given payload and uses Firebase Cloud Storage (bucket),
-// to save it.
+// to save it.  *AND* marks it as the user's current drawing.
 export function saveSerializedDrawing(payload) {
     console.log("XXXX ui handling save")
     const saveDetails = JSON.parse(payload)
     const serializedDrawingAsBlob = new Blob([saveDetails.serializedDrawingBase64Enc]);
     saveDrawing(saveDetails.drawingName, serializedDrawingAsBlob)
+    setCurrentDrawing(drawingName)
 }
