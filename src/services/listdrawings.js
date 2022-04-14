f24e2e7dffc75e2518e96603b72b5a1e885c8efa
peterhoward42 <peterhoward42@gmail.com>
2022-04-14 21:39:11 +0100

    Add dwg already exists service

--- src/services/listdrawings.js
+++ src/services/listdrawings.js
@@ -1,6 +1,15 @@
 import { getDrawingNameIndex } from "../firebase/drawing"
 
+// listUsersDrawings provides a list of the signed-in user's
+// existing drawings.
 export async function listUsersDrawings() {
     const drawingIndex = await getDrawingNameIndex()
     return drawingIndex.keys()
+}
+
+// thisDrawingNameAlreadyExists returns true if the signed-in user already
+// has a drawing of the given name.
+export async function thisDrawingNameAlreadyExists(aDrawingName) {
+    const listOfDrawings = await listUsersDrawings()
+    return listOfDrawings.includes(aDrawingName)
 }
